8168b904a188d965e4f9ce608bb06af05c60bf09
Rob Story <wrobstory@gmail.com>
2013-06-05 00:23:51 -0700

    Fixed example, building README

--- README.md
+++ README.md
@@ -1,63 +1,119 @@
 d3.chart.choropleth
 ===================
 
-# Chart name...
+A d3.chart based choropleth map generator with an easily modified domain and built-in color-brewer colors based on a quantize scale.
 
-Chart description...
+YlGnBu color brew:
+
+![YlGnBu](http://farm4.staticflickr.com/3734/8955684925_da4d2c2c85_o.png)
+
+OrRd color brew:
+
+![OrRd](http://farm8.staticflickr.com/7385/8955684907_3167f47886_o.png)
 
 ### Sample Use
 
-Please provide sample use of your chart here.
+```javascript
+// YlGnBu color brew, custom domain
+var chart_1 = d3.select('#map1')
+  .append("svg")
+  .attr("height", 500)
+  .attr("width", 480)
+  .chart("Choropleth")
+  .domain([0, 15])
+  .range('YlGnBu')
+  .projection(d3.geo.albersUsa())
+  .scale(500);
+
+// OrRd color brew, wider domain
+var chart_2 = d3.select('#map2')
+  .append("svg")
+  .attr("height", 500)
+  .attr("width", 480)
+  .chart("Choropleth")
+  .domain([0, 25])
+  .range('OrRd')
+  .projection(d3.geo.albersUsa())
+  .scale(500);
 
 ### API
 
 Sample API Documentation:
 
-#### `<instance>.highlight(value)`
+#### `<instance>.height(newHeight)`
 
 **Description:**
 
-Allows the highlighting of a specific value
+When value provided sets the height of the SVG. If no value provided, defaults to 500 for bl.ocks integration.
 
 **Parameters:**
 
-* `value` - The value to highlight.
+* `newHeight` - Integer, optional, new height
 
 **Uses:**
 
 Example:
 
 ```javascript
-var chart = d3.select("#vis")
+var map = d3.select("#map")
   .append("svg")
-  .chart("MyEpicChart")
-  .higlight(12);
+  .chart("Choropleth")
+  .height(500);
 ```
 
-### Events
-
-Sample Event Documentation:
-
-#### `brush`
+#### `<instance>.width(newWidth)`
 
 **Description:**
 
-Broadcast when a circle on the chart is being mousedover
+When value provided sets the width of the SVG. If no value provided, defaults to 960 for bl.ocks integration.
 
-**Arguments:**
+**Parameters:**
 
-* `value` - The value corresponding to the circle being mousedover.
+* `newWidth` - Integer, optional, new width
 
 **Uses:**
 
 Example:
 
 ```javascript
-var chart = d3.select("#vis")
+var map = d3.select("#map")
   .append("svg")
-  .chart("MyEpicChart");
-
-chart.on("brush", function(value) {
-  // handle event...
-});
+  .chart("Choropleth")
+  .width(900);
 ```
+
+#### `<instance>.range(newRange)`
+
+**Description:**
+
+When value provided sets the color range for the map, based on the [color brewer](http://colorbrewer2.org/) colors. Must pass one of the following:
+* BuGn
+* BuPu
+* GnBu
+* OrRd
+* PuBu
+* PuBuGn
+* PuRd
+* RdPu
+* YlGn
+* YlGnBu
+* YlOrBr
+* YlOrRd
+
+Defaults to YlGnBu
+
+**Parameters:**
+
+* `newRange` - String, optional, new color range
+
+**Uses:**
+
+Example:
+
+```javascript
+var map = d3.select("#map")
+  .append("svg")
+  .chart("Choropleth")
+  .width(900);
+```
+
--- dist/d3.chart.choropleth.js
+++ dist/d3.chart.choropleth.js
@@ -1,147 +1,149 @@
 /*! d3.chart.choropleth - v0.0.1
  *  License: MIT
- *  Date: 2013-06-04
+ *  Date: 2013-06-05
  */
 (function() {
-d3.chart('Choropleth', {
+  d3.chart('Choropleth', {
 
-    initialize: function() {
+      initialize: function() {
 
-        var chart = this;
+          var chart = this;
 
-        chart.brews = {'BuGn': ['#EDF8FB', '#CCECE6', '#CCECE6', '#66C2A4', '#41AE76',
-                                '#238B45', '#005824'],
-                       'BuPu': ['#EDF8FB', '#BFD3E6', '#9EBCDA', '#8C96C6', '#8C6BB1',
-                                '#88419D', '#6E016B'],
-                       'GnBu': ['#F0F9E8', '#CCEBC5', '#A8DDB5', '#7BCCC4', '#4EB3D3',
-                                '#2B8CBE', '#08589E'],
-                       'OrRd': ['#FEF0D9', '#FDD49E', '#FDBB84', '#FC8D59', '#EF6548',
-                                '#D7301F', '#990000'],
-                       'PuBu': ['#F1EEF6', '#D0D1E6', '#A6BDDB', '#74A9CF', '#3690C0',
-                                '#0570B0', '#034E7B'],
-                       'PuBuGn': ['#F6EFF7', '#D0D1E6', '#A6BDDB', '#67A9CF', '#3690C0',
-                                  '#02818A', '#016450'],
-                       'PuRd': ['#F1EEF6', '#D4B9DA', '#C994C7', '#DF65B0', '#E7298A',
-                                '#CE1256', '#91003F'],
-                       'RdPu': ['#FEEBE2', '#FCC5C0', '#FA9FB5', '#F768A1', '#DD3497',
-                                '#AE017E', '#7A0177'],
-                       'YlGn': ['#FFFFCC', '#D9F0A3', '#ADDD8E', '#78C679', '#41AB5D',
-                                '#238443', '#005A32'],
-                       'YlGnBu': ['#FFFFCC', '#C7E9B4', '#7FCDBB', '#41B6C4', '#1D91C0',
-                                  '#225EA8', '#0C2C84'],
-                       'YlOrBr': ['#FFFFD4', '#FEE391', '#FEC44F', '#FE9929', '#EC7014',
-                                  '#CC4C02', '#8C2D04'],
-                       'YlOrRd': ['#FFFFB2', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A',
-                                  '#E31A1C', '#B10026']};
+          chart.brews = {'BuGn': ['#EDF8FB', '#CCECE6', '#CCECE6', '#66C2A4', '#41AE76',
+                                  '#238B45', '#005824'],
+                         'BuPu': ['#EDF8FB', '#BFD3E6', '#9EBCDA', '#8C96C6', '#8C6BB1',
+                                  '#88419D', '#6E016B'],
+                         'GnBu': ['#F0F9E8', '#CCEBC5', '#A8DDB5', '#7BCCC4', '#4EB3D3',
+                                  '#2B8CBE', '#08589E'],
+                         'OrRd': ['#FEF0D9', '#FDD49E', '#FDBB84', '#FC8D59', '#EF6548',
+                                  '#D7301F', '#990000'],
+                         'PuBu': ['#F1EEF6', '#D0D1E6', '#A6BDDB', '#74A9CF', '#3690C0',
+                                  '#0570B0', '#034E7B'],
+                         'PuBuGn': ['#F6EFF7', '#D0D1E6', '#A6BDDB', '#67A9CF', '#3690C0',
+                                    '#02818A', '#016450'],
+                         'PuRd': ['#F1EEF6', '#D4B9DA', '#C994C7', '#DF65B0', '#E7298A',
+                                  '#CE1256', '#91003F'],
+                         'RdPu': ['#FEEBE2', '#FCC5C0', '#FA9FB5', '#F768A1', '#DD3497',
+                                  '#AE017E', '#7A0177'],
+                         'YlGn': ['#FFFFCC', '#D9F0A3', '#ADDD8E', '#78C679', '#41AB5D',
+                                  '#238443', '#005A32'],
+                         'YlGnBu': ['#FFFFCC', '#C7E9B4', '#7FCDBB', '#41B6C4', '#1D91C0',
+                                    '#225EA8', '#0C2C84'],
+                         'YlOrBr': ['#FFFFD4', '#FEE391', '#FEC44F', '#FE9929', '#EC7014',
+                                    '#CC4C02', '#8C2D04'],
+                         'YlOrRd': ['#FFFFB2', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A',
+                                    '#E31A1C', '#B10026']};
 
-        chart.w = chart.base.attr('width') || 960;
-        chart.h = chart.base.attr('height') || 500;
-        chart._range = chart._range || chart.brews.YlGnBu;
-        chart._domain = chart._domain || 0;
-        chart._path = d3.geo.path();
-        chart._projection = chart._projection || d3.geo.mercator();
-        chart._scale = chart._scale || 1000;
+          chart.w = chart.base.attr('width') || 960;
+          chart.h = chart.base.attr('height') || 500;
+          chart._range = chart._range || chart.brews.YlGnBu;
+          chart._domain = chart._domain || 0;
+          chart._path = d3.geo.path();
+          chart._projection = chart._projection || d3.geo.mercator();
+          chart._scale = chart._scale || 1000;
 
-        chart.quantize = d3.scale.quantize()
-          .domain(chart._domain)
-          .range(chart._range);
+          chart.quantize = d3.scale.quantize()
+            .domain(chart._domain)
+            .range(chart._range);
 
-        function dataBind(data) {
+          function dataBind(data) {
 
-          var chart = this.chart();
-
-          chart.geo_data = data.Geo;
-          chart.data = data.ToBind;
-
-          var getMax = function(data) {
-            var maxVal = -1;
-            var maxId = null;
-            for (var id in data) {
-              var value = data[id];
-              if (value > maxVal) {
-                maxId = id;
-                maxVal = value;
-              }
-            }
-            return maxVal;
-          };
-
-          chart._domain = chart._domain === 0 ? [0, getMax(chart.data)] : chart._domain;
-          chart.quantize.domain(chart._domain).range(chart._range);
-
-          return this.selectAll("path")
-                     .data(chart.geo_data);
-        }
-
-        function insert() {
             var chart = this.chart();
 
-            var scale = chart._scale;
+            chart.geo_data = data.Geo;
+            chart.data = data.ToBind;
 
-            return this.append("path")
-                .style("fill", function (d) { return chart.quantize(chart.data[d.id]); })
-                .attr("d", chart._path.projection(chart._projection
-                                                       .scale(chart._scale)
-                                                       .translate([chart.w / 2, chart.h / 2])));
-        }
+            var getMax = function(data) {
+              var maxVal = -1;
+              var maxId = null;
+              for (var id in data) {
+                var value = data[id];
+                if (value > maxVal) {
+                  maxId = id;
+                  maxVal = value;
+                }
+              }
+              return maxVal;
+            };
 
-        var mapBase = this.base
-          .append('g')
-          .classed('geodata', true);
+            chart._domain = chart._domain === 0 ? [0, getMax(chart.data)] : chart._domain;
+            chart.quantize.domain(chart._domain).range(chart._range);
 
-        chart.layer('map', mapBase, {
-            dataBind: dataBind,
-            insert: insert
-        });
-    },
+            return this.selectAll("path")
+                       .data(chart.geo_data);
+          }
 
-    width: function(newWidth) {
-    if (arguments.length === 0) {
-      return this.w;
-    }
-    this.w = newWidth;
-    return this;
-    },
+          function insert() {
+              var chart = this.chart();
 
-    height: function(newHeight) {
+              var scale = chart._scale;
+
+              return this.append("path")
+                  .style("fill", function (d) { return chart.quantize(chart.data[d.id]); })
+                  .attr("d", chart._path.projection(chart._projection
+                                                         .scale(chart._scale)
+                                                         .translate([chart.w / 2, chart.h / 2])));
+          }
+
+          var mapBase = this.base
+            .append('g')
+            .classed('geodata', true)
+            .attr('height', chart.h)
+            .attr('width', chart.w);
+
+          chart.layer('map', mapBase, {
+              dataBind: dataBind,
+              insert: insert
+          });
+      },
+
+      width: function(newWidth) {
       if (arguments.length === 0) {
-        return this.h;
+        return this.w;
       }
-      this.h = newHeight;
+      this.w = newWidth;
       return this;
-    },
+      },
 
-   range: function(newRange) {
-     if (arguments.length === 0) {
-        return this._range;
+      height: function(newHeight) {
+        if (arguments.length === 0) {
+          return this.h;
+        }
+        this.h = newHeight;
+        return this;
+      },
+
+     range: function(newRange) {
+       if (arguments.length === 0) {
+          return this._range;
+        }
+        this._range = this.brews[newRange];
+        return this;
+      },
+
+     domain: function(newDomain) {
+       if (arguments.length === 0) {
+          return this._domain;
+        }
+        this._domain= newDomain;
+        return this;
+      },
+
+      projection: function(newProj) {
+       if (arguments.length === 0) {
+          return this._projection;
+        }
+        this._projection= newProj;
+        return this;
+      },
+
+      scale: function(newScale) {
+       if (arguments.length === 0) {
+          return this._scale;
+        }
+        this._scale = newScale;
+        return this;
       }
-      this._range = this.brews[newRange];
-      return this;
-    },
 
-   domain: function(newDomain) {
-     if (arguments.length === 0) {
-        return this._domain;
-      }
-      this._domain= newDomain;
-      return this;
-    },
-
-    projection: function(newProj) {
-     if (arguments.length === 0) {
-        return this._projection;
-      }
-      this._projection= newProj;
-      return this;
-    },
-
-    scale: function(newScale) {
-     if (arguments.length === 0) {
-        return this._scale;
-      }
-      this._scale = newScale;
-      return this;
-    }
-
-});
+  });
 }());
